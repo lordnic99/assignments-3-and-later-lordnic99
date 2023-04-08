048e9849317c5e095afd85f8a79609015b95fcc5
lordnic99 <nguyenluongdinhhoang@gmail.com>
2023-04-08 16:39:46 +0700

    Assignment 1 Commit

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# parameters check 
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of parameters!"
+  exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+# directory check 
+if [ ! -d ${filesdir} ]; then
+  echo "Directory ${filesdir} DOES NOT exists!"
+  exit 1
+fi
+
+file_count=$(find ${filesdir} -type f | wc -l)
+line_count=$(grep -r ${searchstr} ${filesdir} 2> /dev/null | wc -l)
+
+echo "The number of files are ${file_count} and the number of matching lines are ${line_count}"
+
+
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -17,6 +17,9 @@ void test_validate_my_username()
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
+     *
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *my_true_username = my_username();
+    const char *my_username_file = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING(my_true_username,my_username_file);
 }
